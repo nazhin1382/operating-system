de9d62e5e0a8307c61a117bcb1512dcc434a9924
nazhin <nazhin.abdolbaghi@gmail.com>
2023-12-22 15:09:06 +0200

    first UI c++ code

---
+++ operating System project.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	// menue for the user
+	cout<<"you can selsect from the menue which scheduler you want to apply on the processes";
+	cout<<endl<<"1-first come first serve (FCFS)";
+	cout<<endl<<"2-Shortest-Job-First (SJF) ";
+	cout<<endl<<"3-Priority Scheduling";
+	cout<<endl<<"4-Round Robin(RR)";
+	int selectedoption;
+	switch(selectedoption)
+	{
+		case 1: fcfs(); break;
+		case 2: sjf(); break;
+		case 3: priority(); break;
+		case 4: rr(); break;
+		default: cout<<"please select an option between 1-4";
+    }
+	return 0;
+}
