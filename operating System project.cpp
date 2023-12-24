86da3ddf2ed4df8d0a2451b2abccabfe9fbd9a81
nazhin <nazhin.abdolbaghi@gmail.com>
2023-12-24 00:32:57 -0900

    two menu added

--- operating System project.cpp
+++ operating System project.cpp
@@ -1,22 +1,54 @@
-#include <iostream>
-using namespace std;
+include <iostream>
+using namespace sd;
+void firstmenu();
+void methodmenu();
+void fcfs();
+void sjf();
+void priority();
+void RR();
 int main ()
 {
-	// menue for the user
-	cout<<"you can selsect from the menue which scheduler you want to apply on the processes";
-	cout<<endl<<"1-first come first serve (FCFS)";
-	cout<<endl<<"2-Shortest-Job-First (SJF) ";
-	cout<<endl<<"3-Priority Scheduling";
-	cout<<endl<<"4-Round Robin(RR)";
-	
-	int selectedoption;
-	// switch(selectedoption)
-	// {
-	// 	case 1: fcfs(); break;
-	// 	case 2: sjf(); break;
-	// 	case 3: priority(); break;
-	// 	case 4: rr(); break;
-	// 	default: cout<<"please select an option between 1-4";
-    // }
-	return 0;
+
+
+
+return 0;
+
+}
+void firstmenu()
+{
+//menue for the user 
+cout<<"CPU Scheduler Simulator"<<endl;
+cout<<"1-Scheduling Method"<<endl;
+cout<<"2-Preemptive Mode"<<endl;
+cout<<"3-Show Result"<<endl;
+cout<<"4-End Program";
+int firstmenuanswer;
+switch(firstmenueanswer)
+ { case 1: methodmenu();
+  case 2: preemtivemode();
+  case 3: result();
+  case 4: exit;
+  default: cout<<"please select between the mene choices (1-4)";
+
+ }
+
+}
+void methodmenu()
+{
+//methodmenue for the user 
+cout<<"1-None of scheduling method chosen""<<endl;
+cout<<"2-First Come,First Served Scheduling"<<endl;
+cout<<"3-Shortest-Job-First Scheduling"<<endl;
+cout<<"4-Priority Scheduling"<<endl;
+cout<<"Round-Robin Scheduling (You should also obtain time quantum value)";
+int methodmenuanswer;
+switch(methodmenuanswer)
+ {case 1: firstmenu();
+  case 2: fcfs();
+  case 3: sjf();
+  case 4: priority();
+  case 5: RR();
+  default: cout<<"please select between the mene choices (1-4)";
+ }
+
 }
