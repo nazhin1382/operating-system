b5701fec312cd5ec73030446fdf67b71e7dfa9b6
nazhin <nazhin.abdolbaghi@gmail.com>
2023-12-24 21:03:59 -0900

    linked list added

--- operating System project.cpp
+++ operating System project.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <unistd.h>
+#include fstream;
 using namespace std;
 void firstmenu();
 void methodmenu();
@@ -7,8 +9,16 @@ void result();
 void fcfs();
 void sjf();
 void priority();
-void RR();
-int main ()
+void rr();
+int getopt(int argc, char * const argv[],const char *optstring);
+struct node * createprocess(string);
+void display processes(struct process *);
+struct node * addprocess(struct process *, string);
+struct process{
+ string data ;
+ struct process *next;
+};
+int main (int argc, char *argv[])
 {
 
 firstmenu();
@@ -51,32 +61,59 @@ switch(methodmenuanswer)
   case 2: fcfs();break;
   case 3: sjf();break;
   case 4: priority();break;
-  case 5: RR();break;
+  case 5: rr();break;
   default: cout<<"please select between the mene choices (1-5)";
  }
 
 }
+void result()
+{
+    firstmenu();    
+}
 void preemtivemode()
 {
-    
+    firstmenu(); 
 }
 void showresult()
 {
-    
+    firstmenu(); 
 }
 void fcfs()
 {
-    
+    firstmenu(); 
 }
 void sjf()
 {
-
+   firstmenu(); 
 }
 void priority()
 {
-    
+    firstmenu(); 
 }
 void rr()
 {
-    
+   firstmenu(); 
+}
+struct process * createprocess(string data)
+{
+     struct process * temp;
+     temp = (struct process *) malloc(sizeof(process));
+     temp->item = data;
+     temp->next = NULL;
+     return temp;
+}
+struct process* insertprocess(struct process *header, string data)
+{
+       struct process * temp = createprocess(data);
+       struct process * headertemp;
+       if (header == NULL)
+       {
+          header = temp;
+          return header;
+       }
+       headertemp=header;
+       while(headertemp->next != NULL)
+            headertemp=headertemp->next;
+       headertemp->next = temp;
+       return header;
 }
